1e71aadd27c4840e54a610ef61e2bcab1d2cc0c5
anapdh <ana_d_hubner@hotmail.com>
2021-03-23 08:23:54 -0300

    Add left sidebar and link to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,5 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-  end  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :opinions
 
   devise_for :users, :controllers => { registrations: 'registrations', users: 'users'}
-  match '/users',   to: 'users#index',   via: 'get'
-  match 'users/:id' => 'users#show', via: :get
+  match '/users',   to: 'users#index', via: 'get'
+  match '/users/:id', to: 'users#show', via: 'get', :as => :users_show
 
   root to: "opinions#index"
 end
