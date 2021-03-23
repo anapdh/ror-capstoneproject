0a9ba763017bba76d1e652ce3a653e3bdc00a644
anapdh <ana_d_hubner@hotmail.com>
2021-03-23 16:46:47 -0300

    Add helper for application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def define_page(info, home)
+    if current_page?(users_show_path(:id))
+      render 'users/user_info'
+    else
+      render 'users/who_to_follow'
+    end
+  end
 end
