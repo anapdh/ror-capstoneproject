362698a7fcdf21d5e9df636b43468cbe3aa6d83f
anapdh <ana_d_hubner@hotmail.com>
2021-03-23 07:06:20 -0300

    Add Users controller with index and show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :opinions
-  devise_for :users, :controllers => { registrations: 'registrations' }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users, :controllers => { registrations: 'registrations', users: 'users'}
+  match '/users',   to: 'users#index',   via: 'get'
+  match 'users/:id' => 'users#show', via: :get
+
   root to: "opinions#index"
 end
