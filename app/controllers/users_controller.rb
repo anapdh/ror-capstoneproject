3668663133ac06df64bf3cb31c5730f7eff1acb7
anapdh <ana_d_hubner@hotmail.com>
2021-03-27 22:41:18 -0300

    Add styles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,12 +13,12 @@ class UsersController < ApplicationController
   def follow
     @user = User.find(params[:id])
     current_user.followees << @user
-    redirect_back(fallback_location: user_path(@user))
+    redirect_back(fallback_location: users_path(@user))
   end
   
   def unfollow
     @user = User.find(params[:id])
     current_user.followed_users.find_by(followee_id: @user.id).destroy
-    redirect_back(fallback_location: user_path(@user))
+    redirect_back(fallback_location: users_path(@user))
   end
 end
