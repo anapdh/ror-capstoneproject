5897727567dd0663b0591789c834e0353da454d5
anapdh <ana_d_hubner@hotmail.com>
2021-03-28 19:51:07 -0300

    Add spec for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:opinions) }
+    it { should have_many(:followers) }
+    it { should have_one_attached(:photo) }
+    it { should have_one_attached(:coverimage) }
+    it { should have_many(:followed_users).class_name('Following') }
+    it { should have_many(:followees) }
+    it { should have_many(:following_users).class_name('Following') }
+    it { should have_many(:followers) }
+  end
+
+  describe 'validations' do
+    it { should validate_uniqueness_of(:username) }
+    it { should validate_presence_of(:username) }
+    it { should validate_length_of(:username) }
+    it { should validate_presence_of(:fullname) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+  end
 end
