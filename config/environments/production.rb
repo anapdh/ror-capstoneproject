c99c7e767f0422f765b2d2d657abfb8d261e2111
anapdh <ana_d_hubner@hotmail.com>
2021-03-29 00:00:03 -0300

    Fix logger in config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.logger = Logger.new(STDOUT)
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
