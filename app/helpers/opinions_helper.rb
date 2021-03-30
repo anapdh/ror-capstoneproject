98c05e23061ba5d10e1e51b7765ba5ff7e042cfb
anapdh <ana_d_hubner@hotmail.com>
2021-03-30 20:49:16 -0300

    Remove logic from views

--- app/helpers/opinions_helper.rb
+++ app/helpers/opinions_helper.rb
@@ -1,7 +1,7 @@
 module OpinionsHelper
   def user_owner?(opinion)
     if user_signed_in? && current_user.id == opinion.user_id
-      render 'user_opinions_only', opinion: opinion
+      render 'opinion_owner', opinion: opinion
     end
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+  def display_followers(user)
+    out = ''
+    if @user.id == current_user.id
+    elsif current_user.followees.include?(@user)
+      out += "<h5 class='btn'> #{button_to 'Unfollow', unfollow_path, method: 'POST'} </h5>"
+    else
+      out += "<h5 class='btn'>#{button_to 'Follow', follow_path, method: 'POST' }</h5>"
+    end
+  out.html_safe
+  end
 end
