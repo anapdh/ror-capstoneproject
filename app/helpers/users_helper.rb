b9110eb81999291905816d2172beb36ea2d84198
anapdh <ana_d_hubner@hotmail.com>
2021-03-30 21:13:08 -0300

    Add helper for users followers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-  def display_followers(user)
+  def display_follow_btn(user)
     out = ''
     if @user.id == current_user.id
     elsif current_user.followees.include?(@user)
@@ -8,5 +8,17 @@ module UsersHelper
       out += "<h5 class='btn'>#{button_to 'Follow', follow_path, method: 'POST' }</h5>"
     end
   out.html_safe
+  end
+
+  def display_followers(user)
+    out = ""
+    if user.followers.exists?
+      out += "<h5>Followed by:</h5><ul>"
+        user.followers.each do |f|
+          out += "<li>@#{f.username}</li>"
+        end
+        out += "</ul>"
+    end
+    out.html_safe
   end
 end
