9499aabac99f5c9c4b6c4c5abd793548df078ce9
anapdh <ana_d_hubner@hotmail.com>
2021-04-01 00:42:04 -0300

    Fix app typos

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,7 @@ class LikesController < ApplicationController
     @like = current_user.likes.new(like_params)
 
     if @like.save
-      redirect_to root_path, notice: 'You liked a opinion.'
+      redirect_to root_path, notice: 'You liked an opinion.'
     else
       redirect_to root_path, alert: 'You cannot like this opinion.'
     end
@@ -13,9 +13,9 @@ class LikesController < ApplicationController
     @like = Like.find(params[:id])
     if @like
       @like.destroy
-      redirect_to root_path, notice: 'You disliked a opinion.'
+      redirect_to root_path, notice: 'You disliked an opinion.'
     else
-      redirect_to root_path, alert: 'You cannot dislike a opinion that you did not like before.'
+      redirect_to root_path, alert: 'You cannot dislike an opinion that you did not like before.'
     end
   end
 
