817403c99d1ba9312c3acb698fbd3813fbd18fe2
anapdh <ana_d_hubner@hotmail.com>
2021-04-01 13:42:31 -0300

    Add index opinion helper and resolve rspec problems

--- app/helpers/opinions_helper.rb
+++ app/helpers/opinions_helper.rb
@@ -1,5 +1,15 @@
 module OpinionsHelper
+  def display_form
+    render 'form' if user_signed_in?
+  end
+
   def user_owner?(opinion)
     render 'opinion_owner', opinion: opinion if user_signed_in? && current_user.id == opinion.user_id
+  end
+
+  def display_opinions
+    @opinions.each do | opinion |
+
+    end
   end
 end
